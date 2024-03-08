1a441fd30a7280f08c0f07ccb3ea9a51a6297cda
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-03-08 10:21:30 +0000

    add arch_graph_parser.py

--- arch_graph_parser.py
+++ arch_graph_parser.py
@@ -1,21 +1,24 @@
+import os.path
 import sys
-import seaborn as sns
 from xil_res.architecture import Arch
-from processing.plot import print_heatmap
+import processing.plot as plot
 import arch.analysis as an
+import scripts.config as cfg
 
 # initialize device
-#device_name = sys.argv[1]
-device_name = 'xczu3eg'
+device_name = sys.argv[1]
 device = Arch(device_name)
 
 # draw tiles_map heatmap
-#parsed_tiles_map = an.parse_tiles_map(device)
-#print_heatmap(parsed_tiles_map)
+parsed_tiles_map = an.parse_tiles_map(device.tiles_map)
+store_path = os.path.join(cfg.Data_path, 'Analysis')
+filename = 'tiles_map'
+plot.print_heatmap_tiles_map(parsed_tiles_map, store_path=store_path, filename=filename)
 
 
 # draw wires_dict heatmap
-parsed_wires_dict = an.parse_wires_dict(device)
-print_heatmap(parsed_wires_dict)
+parsed_wires_dict = an.parse_wires_dict(device.wires_dict)
+store_path = os.path.join(cfg.Data_path, 'Analysis')
+filename = 'wires_dict'
+plot.print_heatmap_wires_dict(parsed_wires_dict, store_path=store_path, filename=filename)
 
-print('hi')
