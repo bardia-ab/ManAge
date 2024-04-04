c486061dc3006ade4069a96f857adee02a8a79a8
bardia-ab <bardia.babaie@gmail.com>
2024-04-04 14:06:47 +0100

    add program.tcl

---
+++ ManAge/tcl/program.tcl
@@ -0,0 +1,16 @@
+# bitstream file
+set bit_file "[lindex $argv 0].bit"
+
+# open hardware manager
+open_hw_manager
+connect_hw_server -url localhost:3121
+current_hw_target [get_hw_targets *]
+open_hw_target
+
+# Program and Refresh the device
+current_hw_device [lindex [get_hw_devices] 0]
+refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
+set_property PROGRAM.FILE $bit_file [lindex [get_hw_devices] 0]
+set_param xicom.use_bitstream_version_check false
+program_hw_devices [lindex [get_hw_devices] 0]
+refresh_hw_device [lindex [get_hw_devices] 0]
