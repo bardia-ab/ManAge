f22f0254c6e1f35354bd2ee8534198fdc70b07a1
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-06-02 15:14:57 +0100

    minor fix

--- ManAge/scripts/populate_cut_delay.py
+++ ManAge/scripts/populate_cut_delay.py
@@ -1,13 +1,41 @@
-import os, sys
+import os, sys, subprocess
 from pathlib import Path
+from tqdm import tqdm
 sys.path.append(str(Path(__file__).parent.parent))
 os.chdir(str(Path(__file__).parent.parent))
+from xil_res.architecture import Arch
 import utility.config as cfg
 
-TCs_path = r'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\Ageing_Experiment\min_bitstreams\X2Y1'
-TCs_result_path = r'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\Ageing_Experiment\Results\minimal\iter0'
+CR = 'X2Y1'
+TCs_path = rf'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\Ageing_Experiment\PV_TCs\split_TC\{CR}'
+TCs_result_path = r'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\Ageing_Experiment\Results\minimal'
 skew_path = None
-store_path = r'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\Ageing_Experiment'
+store_path = r'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\Ageing_Experiment\CUTs_list'
+file_name = f'cuts_list_{CR}.data'
+
+device = Arch('xczu9eg')
 
 script = str(Path(__file__).parent.parent / 'processing' / 'result_process.py')
-os.system(f'{cfg.python} "{script}" {TCs_path} {TCs_result_path} {skew_path} {store_path}')
+iterations = list(Path(TCs_result_path).glob('*'))
+
+# pbar
+pbar = tqdm(total=len(iterations))
+for iter in iterations:
+    pbar.set_description(iter.stem)
+    pbar.set_postfix_str('CUTs List')
+    store_file = Path(store_path) / iter.name / file_name
+    subprocess.run([cfg.python, script, TCs_path, str(iter), str(skew_path), store_file], capture_output=True, text=True, encoding='utf-8')
+
+    pbar.set_postfix_str('Heatmap')
+    cuts_list_file = store_file
+    heatmap_store_path = Path(cuts_list_file).parent.parent.parent / 'heatmap' / iter.name
+    store_file_suffix = 'X2Y1'
+    xmin, xmax, ymin, ymax = device.get_device_dimension()
+    xmin = str(xmin)
+    xmax = str(xmax)
+    ymin = str(ymin)
+    ymax = str(ymax)
+    script = str(Path(__file__).parent.parent / 'processing' / 'delay_heatmap.py')
+    subprocess.run([cfg.python, script, cuts_list_file, heatmap_store_path, store_file_suffix, xmin, xmax, ymin, ymax], capture_output=True,
+                   text=True, encoding='utf-8')
+    pbar.update(1)
