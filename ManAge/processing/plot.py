6a8f9f5bbbe14f065c9313d1cfb4923ad904934e
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-06-02 15:06:22 +0100

    fix issues with cut_delay and heatmap

--- ManAge/processing/plot.py
+++ ManAge/processing/plot.py
@@ -16,7 +16,8 @@ def print_heatmap_tiles_map(input_dict, store_path=None, filename=None, palette=
     reversed_input_dict = {coord: type for type, coords in input_dict.items() for coord in coords}
 
     # print heatmap
-    print_heatmap(reversed_input_dict, all_coords, rows, columns, store_path, filename, palette, xlabel, ylabel)
+    store_file = os.path.join(store_path, filename)
+    print_heatmap(reversed_input_dict, all_coords, rows, columns, store_file, palette, xlabel, ylabel)
 
 def print_heatmap_wires_dict(input_dict, store_path=None, filename=None, palette='pastel', xlabel='FPGA Rows', ylabel='FPGA Columns'):
     # extract types and all_coords
@@ -30,17 +31,17 @@ def print_heatmap_wires_dict(input_dict, store_path=None, filename=None, palette
     reversed_input_dict = {nd.get_coordinate(tile): type for type, tiles in input_dict.items() for tile in tiles}
 
     # print heatmap
-    print_heatmap(reversed_input_dict, all_coords, rows, columns, store_path, filename, palette, xlabel, ylabel)
+    store_file = os.path.join(store_path, filename)
+    print_heatmap(reversed_input_dict, all_coords, rows, columns, store_file, palette, xlabel, ylabel)
 
-def print_heatmap(input_dict, all_coords, rows, columns, store_path, filename, palette, xlabel, ylabel):
-    n_rows, n_columns = len(rows), len(columns)
+def print_heatmap(input_dict, all_coords, rows, columns, store_file, palette, xlabel, ylabel, apply_type=True):
     types = list(set(input_dict.values()))
 
     # create the data matrix
-    data = get_data_matrix(input_dict, all_coords, n_rows, n_columns)
+    data = get_data_matrix(input_dict, all_coords, apply_type)
 
     # create Data Frame
-    df = pandas.DataFrame(data, index=rows, columns=columns)     # origin is in top-left
+    df = pandas.DataFrame(data, index=list(rows), columns=list(columns))     # origin is in top-left
 
     # reverses the y-axis, the origin is in bottem-left
     df = df.iloc[::-1, :]
@@ -52,21 +53,22 @@ def print_heatmap(input_dict, all_coords, rows, columns, store_path, filename, p
     custom_palette = sns.color_palette(palette, len(types))
 
     # plot
-    ax = sns.heatmap(df, mask=mask, cmap=custom_palette, cbar_kws={'ticks': list(range(len(types)))})
+    ax = sns.heatmap(df, mask=mask, cmap=custom_palette)
     ax.set(xlabel=xlabel, ylabel=ylabel)
     plt.xticks(rotation=0)
     plt.yticks(rotation=0)
 
     # Customize color bar
-    cbar = ax.collections[0].colorbar
-    cbar.set_ticks(list(range(len(types))))
-    cbar.set_ticklabels(types)
+    if apply_type:
+        cbar = ax.collections[0].colorbar
+        cbar.set_ticks(list(range(len(types))))
+        cbar.set_ticklabels(types)
 
-    if any(map(lambda x: x is None, {store_path, filename})):
+    if store_file is None:
         plt.show()
 
     else:
-        plt.savefig(os.path.join(store_path, filename), bbox_inches='tight')
+        plt.savefig(store_file, bbox_inches='tight')
 
     # clear the plot
     plt.clf()
@@ -82,17 +84,24 @@ def get_full_range_columns(coords):
     columns = {nd.get_x_coord(coord) for coord in coords}
     return list(range(min(columns), max(columns) + 1))
 
-def get_data_matrix(input_dict, all_coords, n_rows, n_columns):
+def get_data_matrix(input_dict, all_coords, apply_type=True):
+    rows = {nd.get_y_coord(coord) for coord in all_coords}
+    columns = {nd.get_x_coord(coord) for coord in all_coords}
+    n_rows, n_columns = len(rows), len(columns)
+
     types = list(set(input_dict.values()))
     data = [[np.nan] * n_columns for _ in range(n_rows)]
     for coord in all_coords:
-        row = nd.get_y_coord(coord)
-        column = nd.get_x_coord(coord)
-        type = input_dict[coord]
-        try:
-            data[row][column] = types.index(type)
-        except:
-            print(row, column, type)
-            breakpoint()
+        row = nd.get_y_coord(coord) - min(rows)
+        column = nd.get_x_coord(coord) - min(columns)
+        if apply_type:
+            type = input_dict[coord]
+            try:
+                data[row][column] = types.index(type)
+            except:
+                print(row, column, type)
+                breakpoint()
+        else:
+            data[row][column] = input_dict[coord]
 
     return data
