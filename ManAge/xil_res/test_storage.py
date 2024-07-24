3b169b2fa30193d0136685b9ecfa779652ccc92b
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-07-24 19:17:04 +0100

    add support for next iterations

--- ManAge/xil_res/test_storage.py
+++ ManAge/xil_res/test_storage.py
@@ -1,9 +1,13 @@
 import os, time
+import re
+from pathlib import Path
 import networkx as nx
 from dataclasses import dataclass, field
 from itertools import product
 from typing import List, Set
 from tqdm import tqdm
+from itertools import chain
+from joblib import Parallel, delayed
 from xil_res.architecture import Arch
 from xil_res.minimal_config import MinConfig
 from xil_res.clock_domain import ClockDomain, ClockGroup
@@ -12,22 +16,40 @@ import utility.utility_functions as util
 
 @dataclass
 class TestCollection:
-    iteration       :   int
-    desired_tile    :   str
-    queue           :   set             = field(default_factory = Set)
-    TC              :   MinConfig       = field(default = None)
-    device          :   Arch            = field(default = None)
-    clock_domains   :   List            = field(default_factory = list)
-    clock_groups    :   List            = field(default_factory = list)
-    pbar            :   tqdm            = field(default = None)
-    n_pips          :   int             = field(default = 0)
-    TC_idx          :   int             = field(default=0)
+    iteration           :   int
+    origin              :   str
+    minimal_config_dir  :   str
+    prev_config_dir     :   str             = field(default=None)
+    prev_config_files   :   List            = field(default_factory = list)
+    queue               :   set             = field(default_factory = Set)
+    TC                  :   MinConfig       = field(default = None)
+    device              :   Arch            = field(default = None)
+    clock_domains       :   List            = field(default_factory = list)
+    clock_groups        :   List            = field(default_factory = list)
+    pbar                :   tqdm            = field(default = None)
+    n_pips              :   int             = field(default = 0)
+    TC_idx              :   int             = field(default=0)
 
     def __post_init__(self):
-        # create the iteration folder
-        cfg.minimal_config_path = os.path.join(cfg.minimal_config_path, f'iter{self.iteration}')
-        util.create_folder(cfg.minimal_config_path)
+        if self.prev_config_dir is not None:
+            config_files = list(Path(self.prev_config_dir).glob('TC*'))
+            TC_num_CUTs = Parallel(n_jobs=cfg.n_jobs)(delayed(self.get_num_occupied_CUTs)(config_file)
+                                                                  for config_file in config_files)
+            self.sort_prev_TCs(TC_num_CUTs)
+            rloc_collection = util.load_data(str(self.prev_config_dir), 'rloc_collection.data')
+            if f'INT_{self.origin}' in rloc_collection.covered_pips:
+                covered_pips = {tuple(map(lambda node: f'INT_{self.origin}/{node}', pip)) for pip in rloc_collection.covered_pips[f'INT_{self.origin}']}
+            else:
+                covered_pips = set()
 
+            self.queue = self.queue - covered_pips
+
+            self.max_capacity = cfg.max_capacity
+            self.long_TC_process_time = cfg.long_TC_process_time
+
+            self.TC_idx = len(config_files) - 1 # it is incremented in store_TC at the storing of the last config_file
+
+        util.create_folder(self.minimal_config_dir)
         self.create_clock_domains()
         self.create_pbar()
         self.n_pips = len(self.queue)
@@ -44,14 +66,6 @@ class TestCollection:
         # Restore instance attributes (temp_value will be missing)
         self.__dict__.update(state)
 
-    def initialize(self):
-        # create the iteration folder
-        cfg.minimal_config_path = os.path.join(cfg.minimal_config_path, f'iter{self.iteration}')
-        util.create_folder(cfg.minimal_config_path)
-
-        self.create_clock_domains()
-        self.create_pbar()
-        self.n_pips = len(self.queue)
 
     def create_pbar(self):
         custom_format = "{desc}{bar} {percentage:.0f}% | {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}] >> {postfix}"
@@ -78,8 +92,38 @@ class TestCollection:
             clock_group.conflict.add(conflict_CG)
             self.clock_groups.append(clock_group)
 
+    def get_num_occupied_CUTs(self, config_file):
+        TC = util.load_data(str(config_file.parent), config_file.name)
+        num_CUTs = len([cut for cut in TC.D_CUTs if cut.origin == self.origin])
+
+        return (config_file, num_CUTs)
+
+    def sort_prev_TCs(self, TC_num_CUTs):
+        TC_num_CUTs.sort(key=lambda x: x[1])
+        self.prev_config_files = [tup[0] for tup in TC_num_CUTs]
+
     def create_TC(self, device: Arch):
-        TC = MinConfig(device, self.TC_idx)
+        if self.prev_config_files:
+            prev_config_file = self.prev_config_files.pop(0)
+            prev_TC = util.load_data(str(prev_config_file.parent), prev_config_file.name)
+
+            # Modify long_TC_process_time & max_capacity
+            num_existing_CUTs = len([cut for cut in prev_TC.D_CUTs if cut.origin == self.origin])
+            cfg.long_TC_process_time = self.long_TC_process_time - num_existing_CUTs * (cfg.long_TC_process_time / self.max_capacity)
+            cfg.max_capacity = self.max_capacity - num_existing_CUTs
+
+            #Modify TC_idx
+            TC_idx = int(re.findall('\d+', prev_config_file.stem)[0])
+
+        else:
+            if self.prev_config_dir:
+                cfg.max_capacity = self.max_capacity
+                cfg.long_TC_process_time = self.long_TC_process_time
+
+            prev_TC = None
+            TC_idx = self.TC_idx
+
+        TC = MinConfig(device, TC_idx, prev_TC)
         self.TC = TC
         self.device = device
 
@@ -144,13 +188,6 @@ class TestCollection:
         self.pbar.update(prior_length - current_length)
 
     def store_TC(self):
-        util.store_data(cfg.minimal_config_path, f'TC{self.TC.TC_idx}.data', self.TC)
-
-        if self.queue:
-            #util.store_data(cfg.Data_path, f'test_collection.data', self)
-            pass
-        else:
-            if os.path.exists(os.path.join(cfg.Data_path, 'test_collection.data')):
-                os.remove(os.path.join(cfg.Data_path, 'test_collection.data'))
-
-        self.TC_idx += 1
+        util.store_data(self.minimal_config_dir, f'TC{self.TC.TC_idx}.data', self.TC)
+        if not self.prev_config_files:
+            self.TC_idx += 1
