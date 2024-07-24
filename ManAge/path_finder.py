4077e26389b3c0d5ee12ac0af4a8aabf8bd7df98
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-07-24 16:20:09 +0100

    update CLI

--- ManAge/path_finder.py
+++ ManAge/path_finder.py
@@ -1,4 +1,5 @@
 import os.path
+import argparse
 import sys
 from xil_res.test_storage import TestCollection
 from xil_res.architecture import Arch
@@ -6,32 +7,42 @@ from xil_res.path import PathOut, PathIn
 import utility.config as cfg
 import utility.utility_functions as util
 
+# Create Parser
+parser = argparse.ArgumentParser(prog='path_finder', description='Generate the minimal configurations')
+
+# Add Arguments
+parser.add_argument('device_name', choices=Arch.get_models(), help='Specify the fabric of the FPGA')
+parser.add_argument("origin", help='Specify the origin of CUTs')
+parser.add_argument("iteration", type=int, help='Specify the iteration of path finding')
+parser.add_argument('minimal_config_dir', help='Specify the directory to store minimal configurations')
+
+parser.add_argument('-l', '--local', action='store_true', help='Specify the local CUTs in minimal configurations')
+parser.add_argument('-p', '--prev_config_dir', help='Specify the directory of previous stored configurations')
 
 if __name__ == "__main__":
 
-    # User's inputs
-    # usage: python3 path_finder.py device_name desired_tile iteration pips_mode
-    device_name = sys.argv[1]
-    desired_tile    = sys.argv[2]
-    iteration       = int(sys.argv[3])
-    pips_mode       = sys.argv[4]
+    # Parse Arguments
+    args = parser.parse_args()
 
     Empty_TC = False
-    if pips_mode == 'local':
-        cfg.long_TC_process_time = 7
+    if args.local:
+        cfg.long_TC_process_time = cfg.long_TC_process_time_local
+
+    if args.prev_config_dir:
+        cfg.first_iteration = False
 
     # init device
-    device = Arch(device_name)
+    device = Arch(args.device_name)
 
     # set compressed graph
-    device.set_compressed_graph(desired_tile)
+    device.set_compressed_graph(args.origin)
     device.reform_cost()
     device.remove_untested_edges()
     device.weight = PathIn.weight_function(device.G, 'weight')
 
-    pips = device.get_pips(desired_tile, mode=pips_mode)
+    pips = device.get_pips(args.origin, local=args.local)
 
-    test_collection = TestCollection(iteration=iteration, desired_tile=desired_tile, queue=pips)
+    test_collection = TestCollection(args.iteration, args.origin, args.minimal_config_dir, prev_config_dir=args.prev_config_dir, queue=pips)
 
     #create a TC
     while test_collection.queue:
@@ -41,17 +52,18 @@ if __name__ == "__main__":
 
         if TC.CUTs:
             test_collection.store_TC()
+        elif test_collection.prev_config_files:
+            pass
+        elif Empty_TC:
+            device.reset_costs(test_collection)
+            device.reform_cost()
+            Empty_TC = True
         else:
-            if Empty_TC:
-                device.reset_costs(test_collection)
-                device.reform_cost()
-                Empty_TC = True
-            else:
-                break
+            break
 
         # reset weights
         if (test_collection.n_pips - len(test_collection.queue) // test_collection.n_pips) > 0.3:
             device.reset_costs(test_collection)
             device.reform_cost()
 
-    util.store_data(cfg.minimal_config_path, 'test_collection.data', test_collection)
+    util.store_data(args.minimal_config_dir, 'test_collection.data', test_collection)
