86e9931033d04b73d8cb2183467cd6662f080a04
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-08-05 16:07:41 +0100

    rename RO_cluster to RO_network

--- ManAge/RO/RO_wrapper.py
+++ ManAge/RO/RO_wrapper.py
@@ -1,43 +1,42 @@
-import os, sys, shutil
-
+import os, sys, subprocess
 from pathlib import Path
-if not (Path(os.getcwd()).parts[-1] == Path(os.getcwd()).parts[-2] == 'ManAge'):
-    sys.path.append(str(Path(__file__).parent.parent))
-    os.chdir(str(Path(__file__).parent.parent))
+sys.path.append(str(Path(__file__).parent.parent))
+os.chdir(str(Path(__file__).parent.parent))
 
 from xil_res.architecture import Arch
 import utility.config as cfg
 
-# user inputs
+# device
 device_name = 'xczu9eg'
-clk_pips = str(Path(__file__).parent / 'clk_pips.txt')
-config_name = 'TC_full'
+script = Path(__file__).absolute().parent / 'RO_cluster.py'
+filter_tile = 'CLEL_R_X44Y90'
+width = 5
+height = 35
+project_name = f'{width}x{height}_BRAM'
 
-# Scripts
-CWD = Path(__file__).parent
-RO_minima_script = str(CWD / 'RO_minimalTC.py')
-reloc_script = str(CWD / 'RO_reloc.py')
-gen_fsm_script = str(CWD / 'RO_gen_bitstream.py')
+# design
+subcommand = 'design'
+output_fasm_dir = r'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\RO_design\FASM'
+command = f'{cfg.python} "{script}" {subcommand} {device_name} {output_fasm_dir}'
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+if result.returncode != 0:
+    print('Error: ', result.stderr)
 
-# Create RO minimal TC
-for iteration, desired_tile in zip(range(1, 4), ['INT_X46Y90', 'INT_X45Y90', 'INT_X44Y90']):
-    os.system(f'{cfg.python} "{RO_minima_script}" {device_name} {desired_tile} {iteration} "{clk_pips}"')
+# cluster
+subcommand = 'cluster'
+input_fasm_dir = output_fasm_dir
+output_clustered_fasm_dir = rf'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\RO_design\Cluster\{project_name}'
+command = f'{cfg.python} "{script}" {subcommand} {device_name} {input_fasm_dir} {width} {height} {output_clustered_fasm_dir} -f {filter_tile}'
+result = subprocess.run(command, shell=True, capture_output=False, text=True)
+if result.returncode != 0:
+    print('Error: ', result.stderr)
 
-# Relocation
-iteration = 1
-device = Arch(device_name)
-desired_tiles = device.get_INTs()
-os.system(f'{cfg.python} "{reloc_script}" {device_name} {iteration} {config_name} {" ".join(desired_tiles)}')
-
-
-CWD = Path(__file__).parent
-reloc_script = str(CWD / 'RO_reloc.py')
-gen_fsm_script = str(CWD / 'RO_gen_bitstream.py')
-
-
-# generate FASM & bitstreamc
-TC_path = r'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\Data_xczu9eg_RO\Configurations\iter1'
-blank_bitstream = r"C:\Users\t26607bb\Desktop\CPS_Project\RO_Python\bitstream\zero_blank_zu9eg_jtag.bit"
-pyteman_path = r'C:\Users\t26607bb\Desktop\Pyteman\pyteman_dist\fasm2bit.py'
-removed_pips_file = str(CWD / 'removed_pips.txt')
-os.system(f'{cfg.python} "{gen_fsm_script}" {TC_path} {config_name} {blank_bitstream} {pyteman_path} {removed_pips_file}')
+# bitstream
+subcommand = 'bitstream'
+input_clustered_fasm_dir = output_clustered_fasm_dir
+blank_bitstream_file = r"C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\RO_design\blank_zu9eg_jtag.bit"
+output_bitstream_dir = rf'C:\Users\t26607bb\Desktop\CPS_Project\Path_Search\RO_design\Bitstream\{project_name}'
+command = f'{cfg.python} "{script}" {subcommand} {device_name} {input_clustered_fasm_dir} {blank_bitstream_file} {output_bitstream_dir}'
+result = subprocess.run(command, shell=True, capture_output=False, text=True)
+if result.returncode != 0:
+    print('Error: ', result.stderr)
---
+++ ManAge/RO_network.py
@@ -0,0 +1,73 @@
+import argparse
+from tqdm import tqdm
+from joblib import Parallel, delayed
+from pathlib import Path
+from RO_Functions import design, cluster, gen_bitstream
+from xil_res.architecture import Arch
+import utility.config as cfg
+
+# Create parser
+parser = argparse.ArgumentParser(prog='RO_cluster', description='Design RO network')
+subparser = parser.add_subparsers(title='subcommands', dest='subcommand')
+
+# Create a parent parser
+parent_parser = argparse.ArgumentParser(add_help=False)
+parent_parser.add_argument('device_name', choices=Arch.get_models(), help='Specify the fabric of the FPGA')
+
+# Subcommand: design
+parser_design = subparser.add_parser('design', parents=[parent_parser], help='Design RO nwteork for each type of coordinates')
+parser_design.add_argument('output_fasm_dir', help='Path to the output FASM files directory')
+
+parser_design.add_argument('-c', '--clk_pips_file', help='Specify the path to clock pips file. These pips are excluded from the design.')
+parser_design.add_argument('-r', '--remove_pips_file', help='Specify the path to a pips file that must be excluded from the design.')
+
+# Subcommand: cluster
+parser_cluster = subparser.add_parser('cluster', parents=[parent_parser], help='Cluster the whole fabric into specified window size.')
+parser_cluster.add_argument('input_fasm_dir', help='Path to the input FASM files directory')
+parser_cluster.add_argument('size', type=int, nargs=2, help='Specify the size of the window (Width, Height)')
+parser_cluster.add_argument('output_fasm_dir', help='Path to the output FASM files directory')
+
+parser_cluster.add_argument('-f', '--filter', help='Specify the CLB tile whose encompassing window is desired')
+
+# Subcommand: bitstream
+parser_bitstream = subparser.add_parser('bitstream', parents=[parent_parser], help='Generate bitstreams for FASM files')
+parser_bitstream.add_argument('input_fasm_dir', help='Path to the input FASM files directory')
+parser_bitstream.add_argument('blank_bitstream_file', help='Path to the blank bitstream file')
+parser_bitstream.add_argument('output_bitstream_dir', help='Path to the output bitstream files directory')
+
+
+if __name__ == '__main__':
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Device
+    device = Arch(args.device_name, non_clb_tiles=True)
+
+    if args.subcommand == 'design':
+        # create folder
+        Path(args.output_fasm_dir).mkdir(parents=True, exist_ok=True)
+
+        design(args, device)
+
+    elif args.subcommand == 'cluster':
+        # create folder
+        Path(args.output_fasm_dir).mkdir(parents=True, exist_ok=True)
+
+        cluster(args, device)
+
+    elif args.subcommand == 'bitstream':
+        # create folder
+        Path(args.output_bitstream_dir).mkdir(parents=True, exist_ok=True)
+
+        # Create pbar
+        fasm_files = list(Path(args.input_fasm_dir).glob('*.fasm'))
+        pbar = tqdm(total=len(fasm_files))
+
+        Parallel(n_jobs=cfg.n_jobs, require='sharedmem')(delayed(gen_bitstream)(cfg.pyteman_path, fasm_file, args.blank_bitstream_file, args.output_bitstream_dir, pbar) for fasm_file in fasm_files)
+
+        
+    else:
+        parser.print_help()
+
+
