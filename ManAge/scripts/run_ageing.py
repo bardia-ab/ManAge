94943e7d82754d254c9e84002f00411a917c574a
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-08-12 12:48:49 +0100

    update ageing script

--- ManAge/scripts/run_ageing.py
+++ ManAge/scripts/run_ageing.py
@@ -1,18 +1,88 @@
 import sys, os, time
 from pathlib import Path
-if not (Path(os.getcwd()).parts[-1] == Path(os.getcwd()).parts[-2] == 'ManAge'):
-    sys.path.append(str(Path(__file__).parent.parent))
-    os.chdir(str(Path(__file__).parent.parent))
+from tqdm import tqdm
+sys.path.append(str(Path(__file__).parent.parent))
+os.chdir(str(Path(__file__).parent.parent))
 
-import utility.config as cfg
+from Ageing_Experiment.Ageing import Ageing
 
-script = Path(__file__).parent.parent / 'Ageing_Experiment' / 'Ageing.py'
-RO_bitstream = '/home/bardia/Downloads/osci_lut6c_flood_stage2_wwee12_ff_unit_9eg_BLANKLUT_60clb'
-blank_bitstream = '/home/bardia/Downloads/blank_zu9eg_jtag'
-minimal_char_bitstream_path = '/home/bardia/Desktop/bardia/Timing_Characterization/Backup/Data_xczu9eg/Bitstreams/X2Y1'
-full_char_bitstream_path = '/home/bardia/Desktop/bardia/Timing_Characterization/CR_X2Y1/Bitstreams'
-store_path = '/home/bardia/Desktop/bardia/ManAge_Data/Ageing_Experiment'
-cycles = 4
+def clear_last_line():
+    # Clears the last line in the terminal
+    sys.stdout.write('\x1b[1A')  # move cursor up to the last line
+    sys.stdout.write('\x1b[2K')  # clear line
+    sys.stdout.flush()
 
-command = f'{cfg.python} {str(script)} {RO_bitstream} {blank_bitstream} {minimal_char_bitstream_path} {full_char_bitstream_path} {store_path} {cycles}'
-os.system(command)
+
+RO_bitstream = '/home/bardia/Downloads/osci_lut6c_flood_stage2_wwee12_ff_unit_9eg_BLANKLUT_60clb.bit'
+blank_bitstream = '/home/bardia/Downloads/blank_zu9eg_jtag.bit'
+N_Parallel = 50
+cycles = 14
+
+ageing_exp = Ageing(RO_bitstream, blank_bitstream, N_Parallel)
+
+# set UART
+baud_rate = 230400
+serial_port = '/dev/ttyUSB0'
+ageing_exp.set_UART(baud_rate, serial_port)
+
+# set timing
+initial_heatup_time = 15    # mins
+initial_recovery_time = 15  # mins
+burning_time = 16           # hours
+recovery_time = 30          # mins
+one_day = 24 * 60 * 60
+
+ageing_exp.set_timing(initial_heatup_time, initial_recovery_time, burning_time, recovery_time)
+
+# set minimal characterization paths
+min_vivado_srcs_dir = '/home/bardia/Desktop/bardia/Timing_Characterization/Backup/Data_xczu9eg/Vivado_Sources/X2Y1'
+min_bitstreams_dir = '/home/bardia/Desktop/bardia/Timing_Characterization/Backup/Data_xczu9eg/Bitstreams/X2Y1'
+min_results_dir = '/home/bardia/Desktop/bardia/ManAge_Data/Ageing_Experiment/Results_12_08_2024/minimal_char'
+
+ageing_exp.set_min_char(min_vivado_srcs_dir, min_bitstreams_dir, min_results_dir)
+
+# set full characterization paths
+full_vivado_srcs_dir = '/home/bardia/Desktop/bardia/Timing_Characterization/CR_X2Y1/Vivado_Sources/X2Y1'
+full_bitstreams_dir = '/home/bardia/Desktop/bardia/Timing_Characterization/CR_X2Y1/Bitstreams/X2Y1'
+full_results_dir = '/home/bardia/Desktop/bardia/ManAge_Data/Ageing_Experiment/Results_12_08_2024/full_char'
+
+ageing_exp.set_min_char(full_vivado_srcs_dir, full_bitstreams_dir, full_results_dir)
+
+pbar = tqdm(total=cycles)
+
+for iteration in range(cycles):
+    pbar.set_description(f'Iteration: {iteration}')
+
+    init = (iteration == 0)
+
+    #heat up
+    pbar.set_postfix_str('Burning')
+    ageing_exp.heatup(init)
+
+    #recovery
+    pbar.set_postfix_str('Recovery')
+    ageing_exp.recovery(init)
+
+    #minimal characterization
+    pbar.set_postfix_str('Minimal Characterization')
+    ageing_exp.characterize(type='min')
+
+    #full characterization
+    pbar.set_postfix_str('Full Characterization')
+    ageing_exp.characterize(type='full')
+
+    # increment iteration
+    ageing_exp.increment()
+
+    pbar.update(1)
+
+# one day recovery
+time.sleep(one_day)
+
+# minimal characterization
+pbar.set_postfix_str('Final Minimal Characterization')
+ageing_exp.characterize(type='min')
+
+# full characterization
+pbar.set_postfix_str('Final Full Characterization')
+ageing_exp.characterize(type='full')
