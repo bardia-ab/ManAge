8c164e4b0fbfe8b95afb5738dd79f25e72fa84fb
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-08-12 11:00:12 +0100

    add read & write methods

--- ManAge/experiment/read_packet.py
+++ ManAge/experiment/read_packet.py
@@ -1,6 +1,60 @@
+import threading, serial, time
 class Read:
 
-    def __init__(self):
+    def __init__(self, port):
+        self.port = port
         self.packet = None
+
     def read_data(self, port):
         self.packet = port.read_until(b'END')
+
+
+    def run_exp(self, type='Both'):
+        print(f'Serial Port: {self.port.name}\t{type} Transitions')
+
+        # Start reading thread
+        read_thread = threading.Thread(target=Read.read_data, args=(self, self.port))
+        read_thread.start()
+
+        # Clear IO buffers
+        self.port.reset_input_buffer()
+        self.port.reset_output_buffer()
+
+        # Reset
+        self.reset()
+
+        # Set transition
+        self.set_trans(type)
+
+        # Start
+        self.start()
+
+        # Wait for the read thread to complete
+        read_thread.join()
+
+        # Reset
+        self.reset()
+
+        return list(self.packet[:-3])
+
+    def reset(self):
+        self.port.write(b'R')
+        self.port.flush()
+        time.sleep(0.5)
+
+    def start(self):
+        self.port.write(b'S')
+        self.port.flush()
+        time.sleep(0.5)
+
+    def set_trans(self, type='Both'):
+        if type == 'Rising':
+            trans = b'U'
+        elif type == 'Falling':
+            trans = b'D'
+        else:
+            trans = b'B'
+
+        self.port.write(trans)
+        self.port.flush()
+        time.sleep(0.5)
