1f1b0f561b628708b66de41ec16d0a73c6892913
bardia-ab <bardia.babaei@postgrad.manchester.ac.uk>
2024-08-17 18:51:07 +0100

    update script

--- ManAge/tcl/generate_bitstream.tcl
+++ ManAge/tcl/generate_bitstream.tcl
@@ -1,11 +1,63 @@
-proc gen_bit {Proj_Dir Vivado_Sources_Dir Data_Dir CR TC_Name N_Parallel} {
-	set CUTs "$Vivado_Sources_Dir/$TC_Name/CUTs.vhd"
-	set constraint "$Vivado_Sources_Dir/$TC_Name/physical_constraints.xdc"
-	set stats "$Vivado_Sources_Dir/$TC_Name/stats.txt"
-	set Proj_Name [get_proj_name $Proj_Dir]
-	set bd_file "$Proj_Dir/$Proj_Name\.srcs/sources_1/bd/design_1/design_1.bd"
-	set bitstream_file "$Data_Dir/Bitstreams/$CR/$TC_Name\.bit"
-	set DCP_file "$Data_Dir/DCPs/$CR/$TC_Name\.dcp"
+# Procedure to parse command-line arguments
+proc parse_args {argv valid_flags valid_options cmd_exist} {
+    set cmd ""
+    set positional_args [list]
+    set options [dict create]
+
+    # Initialize all flags to 0 (absent)
+    foreach flag [dict keys $valid_flags] {
+        dict set options $flag 0
+    }
+
+    # Check if at least one argument is provided
+    if {[llength $argv] == 0} {
+        puts "Error: No command provided"
+        return -code error
+    }
+
+    # Extract the subcommand (first argument)
+    if {$cmd_exist} {
+        set cmd [lindex $argv 0]
+        set argv [lrange $argv 1 end]
+    }
+
+    set i 0
+    while {$i < [llength $argv]} {
+        set arg [lindex $argv $i]
+        
+        if {[dict exists $valid_flags $arg]} {
+            # Handle flags (no value expected)
+            dict set options $arg 1
+        } elseif {[dict exists $valid_options $arg]} {
+            # Handle options (value expected)
+            if {$i + 1 < [llength $argv]} {
+                set option_value [lindex $argv [expr {$i + 1}]]
+                if {[string match -* $option_value]} {
+                    puts "Error: Missing value for option '$arg'"
+                    return -code error
+                }
+                dict set options $arg $option_value
+                incr i
+            } else {
+                puts "Error: Missing value for option '$arg'"
+                return -code error
+            }
+        } else {
+            # Positional argument detected
+            lappend positional_args $arg
+        }
+        incr i
+    }
+
+    # Return the parsed command, positional arguments, and options
+    return [list $cmd $positional_args $options]
+}
+
+# Procedure to generate the bitstream
+proc gen_bitstream {proj_dir proj_name vivado_sources_dir N_Segments N_Parallel N_Partial bitstream_file dcp_file} {
+		set CUTs "$vivado_sources_dir/CUTs.vhd"
+		set constraint "$vivado_sources_dir/physical_constraints.xdc"
+		set bd_file "$proj_dir/$proj_name\.srcs/sources_1/bd/design_1/design_1.bd"
 
 	# delete previous implementation
 	catch {reset_run synth_1}
@@ -26,7 +78,6 @@ proc gen_bit {Proj_Dir Vivado_Sources_Dir Data_Dir CR TC_Name N_Parallel} {
 
 	# replace new N_Segments & N_Partial
 	open_bd_design "$bd_file"
-	lassign [get_segmentation "$stats"] N_Segments N_Partial
 	set_property -dict [list CONFIG.g_N_Parallel $N_Parallel CONFIG.g_N_Segments $N_Segments CONFIG.g_N_Partial $N_Partial] [get_bd_cells top_0]
 
 	# validate board design
@@ -37,22 +88,11 @@ proc gen_bit {Proj_Dir Vivado_Sources_Dir Data_Dir CR TC_Name N_Parallel} {
 	catch {launch_runs impl_1 -jobs 12}
 	catch {wait_on_run impl_1}
 
-	# disable version check
-	set_param xicom.use_bitstream_version_check false
-	
 	# store bitstream & DCP
 	open_run impl_1
 	set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
 	write_bitstream -force $bitstream_file
-	write_checkpoint -force $DCP_file
-
-}
-
-proc get_proj_name {Proj_Dir} {
-	set Proj_File [glob -dir $Proj_Dir *.xpr]
-	set Proj_Name [lindex [split [lindex [split $Proj_File /] end] .] 0]
-
-	return $Proj_Name
+	write_checkpoint -force $dcp_file
 }
 
 proc replace_VHDL_src {current_src} {
@@ -81,55 +121,64 @@ proc get_segmentation {stats} {
 	return [list $N_Segments $N_Partial]
 }
 
-# user inputs
-set Vivado_Sources_Dir [lindex $argv 0]
-set Proj_Dir [lindex $argv 1]
-set Data_Dir [lindex $argv 2]
-set CR [lindex $argv 3]
-set TC_Start_Idx [lindex $argv 4]
-set N_TCs [lindex $argv 5]
-set N_Parallel [lindex $argv 6]
-set Even_Odd_Opt [lindex $argv 7]
+# Set options
+set valid_flags [dict create "-overwrite" 1]
+set valid_options [dict create]
+set cmd_exist 0
 
-set Proj_Name [get_proj_name $Proj_Dir]
-set logfile "$Proj_Dir/$Proj_Name\.runs/impl_1/runme.log"
 
-# open project
-open_project "$Proj_Dir/$Proj_Name\.xpr"
+# Parse options
+set parsed_data [parse_args $argv $valid_flags $valid_options $cmd_exist]
+set cmd [lindex $parsed_data 0]
+set positional_args [lindex $parsed_data 1]
+set options [lindex $parsed_data 2]
+
+# Assign positional_args
+set proj_file [lindex $positional_args 0]
+set vivado_sources_dir [lindex $positional_args 1]
+
+set N_Segments [lindex $positional_args 2]
+set N_Parallel [lindex $positional_args 3]
+set N_Partial [lindex $positional_args 4]
+
+set bitstream_file [lindex $positional_args 5]
+set dcp_file [lindex $positional_args 6]
+set log_file [lindex $positional_args 7]
+
+####
+# puts $proj_file
+# puts $vivado_sources_dir
+# puts $N_Segments
+# puts $N_Parallel
+# puts $N_Partial
+# puts $bitstream_file
+# puts $dcp_file
+# puts $log_file
+# puts $options
+# exit
+####
+
+# Extract project directory and file name
+set proj_name [file rootname [file tail $proj_file]]
+set proj_dir [file dirname $proj_file]
+
+if {[catch {current_project $proj_name} result]} {
+	# open project
+	if {[catch {open_project "$proj_file"} result]} {
+		puts "Specified Xilinx project file is invalid!\n$proj_dir"
+	}
+} 
 
 # change severity of critical warnings to error
 set_msg_config -severity {CRITICAL WARNING} -new_severity ERROR
 
-
-if {$Even_Odd_Opt == {even}} {
-	set Even_Odd_TC_Files [glob -dir $Src_Dir *even]
-	lappend Even_Odd_TC_Files {*}[glob -dir $Src_Dir *odd]
-	set Even_Odd_TC_Files [lsort $Even_Odd_TC_Files]
-
-	foreach TC_even_odd $Even_Odd_TC_Files {
-		set TC_Name [lindex [split $TC_even_odd "/"] end]
-		set logfile_dest "$Store_path/Logs/$CR/$TC_Name\.log"
-
-		catch {gen_bit $Proj_Dir $Src_Dir $Store_path $CR $TC_Name $N_Parallel}
-		catch {file copy $logfile $logfile_dest}
-	}
-} elseif {$Even_Odd_Opt == {custom}} {
-	set TCs [glob -dir $Src_Dir *]
-	set TCs [lsort $TCs]
-	
-	foreach TC $TCs {
-		set TC_Name [lindex [split $TC "/"] end]
-		set logfile_dest "$Store_path/Logs/$CR/$TC_Name\.log"
-
-		catch {gen_bit $Proj_Dir $Src_Dir $Store_path $CR $TC_Name $N_Parallel}
-		catch {file copy $logfile $logfile_dest}
-	}
-} else {
-	for {set i $TC_Start_Idx} {$i < $N_TCs} {incr i} {
-		set TC_Name "TC$i"
-		set logfile_dest "$Data_Dir/Logs/$CR/$TC_Name\.log"
-
-		catch {gen_bit $Proj_Dir $Vivado_Sources_Dir $Data_Dir $CR $TC_Name $N_Parallel}
-		catch {file copy $logfile $logfile_dest}
-	}
+if {[file exists $log_file] && ![dict get $options "-overwrite"]} {
+			exit
 }
+
+# Implement design
+gen_bitstream $proj_dir $proj_name $vivado_sources_dir $N_Segments $N_Parallel $N_Partial $bitstream_file $dcp_file
+
+# Copy the log file
+set default_log_file "$proj_dir/$proj_name\.runs/impl_1/runme.log"
+file copy -force $default_log_file $log_file
