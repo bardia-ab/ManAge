bc67486f7601cbfa830e79fa350b6dcdd95f9907
bardia-ab <bardia.babaie@gmail.com>
2024-10-30 14:44:55 +0000

    add docstrings

--- ManAge/xil_res/edge.py
+++ ManAge/xil_res/edge.py
@@ -1,6 +1,6 @@
 import re
 import networkx as nx
-from typing import Tuple
+from typing import Tuple, Set
 from xil_res.node import Node as nd
 import utility.config as cfg
 
@@ -37,6 +37,11 @@ class Edge:
             return self[self.idx - 1]
 
     def get_type(self):
+        """This function returns the type of the Edge (pip|wire)
+
+        :return: Edge type
+        :rtype: str
+        """
         if nd.get_tile(self.name[0]) == nd.get_tile(self.name[1]):
             return 'pip'
         else:
@@ -51,73 +56,46 @@ class PIP(Edge):
         self.reversed_pip   = self.get_reversed_pip(G)
 
     def get_neigh_v(self, G) -> str:
+        """This function returns the neighbor of the head node in (u, v)
+
+        :param G: Architecture Graph
+        :type G: nx.DiGraph
+        :return: the neighbor node
+        :rtype: str
+        """
         return next(G.neighbors(self.name[1]))
 
     def get_pred_u(self, G) -> str:
+        """This function returns the predecessor of the tail node in (u, v)
+
+        :param G: Architecture Graph
+        :type G: nx.DiGraph
+        :return: the predecessor node
+        :rtype: str
+        """
         return next(G.predecessors(self.name[0]))
 
-    def get_reversed_pip(self, G: nx.DiGraph):
+    def get_reversed_pip(self, G: nx.DiGraph) -> Tuple[str] | None:
+        """This function returns the reversed PIP if it exists in the specified graph
+
+        :param G: Architecture Graph
+        :type G: nx.DiGraph
+        :return: Reversed PIP
+        :rtype: Tuple[str] | None
+        """
         reversed_pip = (self.name[1], self.name[0])
         if reversed_pip in G.edges:
             return reversed_pip
         else: return None
 
-    def get_invalid_FF_route_thru_nodes2(self, TC):
-        block_nodes = set()
-        LUT_primitive, LUT_capacity, occupancy = None, None, None
-        if nd.get_clb_node_type(self.neigh_v) == 'LUT_in':
-            #LUT_primitive = next(TC.filter_LUTs(name=nd.get_bel(self.neigh_v)))
-            LUT_primitive = TC.LUTs[nd.get_bel(self.neigh_v)]
-            LUT_capacity = LUT_primitive.capacity
-            occupancy = 2 if (cfg.LUT_in6_pattern.match(self.neigh_v)) else 1
+    def get_invalid_FF_route_thru_nodes(self, TC) -> Set[str]:
+        """This function returns a set of CLB nodes which must be excluded from routing
 
-            if LUT_capacity == 1:
-                attributes = {'get_clb_node_type': 'CLB_muxed',
-                              'get_clock_group': nd.get_clock_group(self.neigh_v),
-                              'get_label': nd.get_label(self.neigh_v)
-                              }
-                block_nodes.update(TC.filter_nodes(**attributes))
-
-        if nd.get_clb_node_type(self.pred_u) in {'CLB_out', 'CLB_muxed'}:
-            #LUT_primitive = next(TC.filter_LUTs(name=nd.get_bel(self.pred_u)))
-            LUT_primitive = TC.LUTs[nd.get_bel(self.pred_u)]
-            LUT_capacity = LUT_primitive.capacity
-            occupancy = 2 if (cfg.MUXED_CLB_out_pattern.match(self.pred_u)) else 1
-
-            if LUT_capacity == 1:
-                attributes = {'get_clb_node_type': 'LUT_in',
-                              'get_bel_index' : 6,
-                              'get_clock_group': nd.get_clock_group(self.pred_u),
-                              'get_label': nd.get_label(self.pred_u)
-                              }
-                block_nodes.update(TC.filter_nodes(**attributes))
-
-            if nd.get_clb_node_type(self.pred_u) == 'CLB_out':
-                block_nodes.update(TC.filter_nodes(get_clb_node_type='CLB_out'))
-                block_nodes = block_nodes - {self.pred_u}
-
-        if LUT_primitive is None:
-            block_nodes.update(filter(cfg.LUT_in_pattern.match, TC.G))
-        else:
-            if LUT_capacity == occupancy:
-                attributes = {'get_clb_node_type': 'FF_out',
-                              'get_clock_group': LUT_primitive.clock_group,
-                              'get_label': LUT_primitive.label
-                              }
-
-                block_nodes.update(TC.filter_nodes(**attributes))
-
-            elif occupancy > LUT_capacity:
-                attributes = {'get_clb_node_type': 'LUT_in',
-                              'get_clock_group': LUT_primitive.clock_group,
-                              'get_label': LUT_primitive.label
-                              }
-
-                block_nodes.update(TC.filter_nodes(**attributes))
-
-        return block_nodes
-
-    def get_invalid_FF_route_thru_nodes(self, TC):
+        :param TC: Minimal test configuration
+        :type TC: MinConfig
+        :return: A set of CLB nodes which must be blocked
+        :rtype: Set[str]
+        """
         block_nodes = set()
         try:
             CLB_node = next(filter(lambda node: nd.get_tile_type(node) == 'CLB', {self.pred_u, self.neigh_v}))
@@ -163,7 +141,12 @@ class PIP(Edge):
 
         return block_nodes
 
-    def get_route_thru_flag(self):
+    def get_route_thru_flag(self) -> bool:
+        """This function specifies if the PIP is immediately connected to a CLB or not
+
+        :return: True|False
+        :rtype: bool
+        """
         route_thru_flag = any(map(lambda x: nd.get_tile_type(x) == 'CLB', {self.neigh_v, self.pred_u}))
 
         return route_thru_flag
